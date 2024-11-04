67f22a61efff2d08d914795352e7b3004e4f0707
Cy J Stavros <cstavros@node202.cluster>
2024-11-04 01:13:09 -0500

    wrapping up for the night

--- transcriptomics/docs/homework2_stavros.R
+++ transcriptomics/docs/homework2_stavros.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 library(DESeq2)
+library(dplyr)
+library(tidyr)
+
 
 options(bitmapType = "cairo")
 
@@ -59,7 +62,7 @@ resultsNames(dds)
 ######################## Contrasts ###############################
 #set up groups within DESeq object
 dds$group <- factor(paste0(dds$DevTemp, dds$FinalTemp))
-design(dds) <- ~ group
+design(dds) <- ~ group #unsure whether or not i need to modify this
 dds <- DESeq(dds)
 dim(dds)
 resultsNames(dds)
@@ -67,3 +70,137 @@ resultsNames(dds)
 # [1] "Intercept"               "group_D18A33_vs_D18A28"  *"group_D18BASE_vs_D18A28"* "group_D22A28_vs_D18A28" 
 # [5] "group_D22A33_vs_D18A28"  "group_D22BASE_vs_D18A28"
 
+#1. compare gene expression between D18BASE and D18A23
+
+res_D18_BASE_D18_A28 <- results(dds, contrast=c("group", "D18BASE", "D18A28"), alpha = 0.05) #filtering for significance
+res_D18_BASE_D18_A28 <- res_D18_BASE_D18_A28[!is.na(res_D18_BASE_D18_A28$padj),] #filtering out NA pvals
+res_D18_BASE_D18_A28 <- res_D18_BASE_D18_A28[order(res_D18_BASE_D18_A28$padj),] #ordering based on pvals
+head(res_D18_BASE_D18_A28)
+summary(res_D18_BASE_D18_A28)
+
+# out of 35524 with nonzero total read count
+# adjusted p-value < 0.05
+# LFC > 0 (up)       : 11, 0.031%
+# LFC < 0 (down)     : 30, 0.084%
+# outliers [1]       : 0, 0%
+# low counts [2]     : 0, 0%
+
+#note to self here: come back and be sure you know which group up and down reg is in reference to.
+
+#make a list of which genes in our comparisons of interest are differentially expressed (list of DEGs)
+degs_D18_BASE_D18_A28 <- row.names(res_D18_BASE_D18_A28[res_D18_BASE_D18_A28$padj < .05,])
+plotMA(res_D18_BASE_D18_A28, ylim=c(-4,4))
+
+#2. compare gene expression between D18BASE and D18A33
+
+res_D18_BASE_D18_A33 <- results(dds, contrast=c("group", "D18BASE", "D18A33"), alpha = 0.05) #filtering for significance
+res_D18_BASE_D18_A33 <- res_D18_BASE_D18_A33[!is.na(res_D18_BASE_D18_A33$padj),] #filtering out NA pvals
+res_D18_BASE_D18_A33 <- res_D18_BASE_D18_A33[order(res_D18_BASE_D18_A33$padj),] #ordering based on pvals
+head(res_D18_BASE_D18_A33)
+summary(res_D18_BASE_D18_A33)
+
+# out of 35524 with nonzero total read count
+# adjusted p-value < 0.05
+# LFC > 0 (up)       : 92, 0.26%
+# LFC < 0 (down)     : 240, 0.68%
+# outliers [1]       : 0, 0%
+# low counts [2]     : 0, 0%
+
+#make a list of which genes in our comparisons of interest are differentially expressed (list of DEGs)
+degs_D18_BASE_D18_A33 <- row.names(res_D18_BASE_D18_A33[res_D18_BASE_D18_A33$padj < .05,])
+plotMA(res_D18_BASE_D18_A33, ylim=c(-4,4))
+
+length(degs_D18_BASE_D18_A28)
+#41 differentially expressed genes
+length(degs_D18_BASE_D18_A33)
+#296 DEGs
+
+#look at the overlaps in which genes are differentially expressed in both contrasts
+length(intersect(degs_D18_BASE_D18_A28, degs_D18_BASE_D18_A33))
+#34 genes differentially expressed in both contrasts
+
+#making euler plots
+
+41-34 #7 transcripts unique to BASE vs 28
+296-34 #262 transcripts unique to BASE vs 33
+
+myEuler <- euler(c("A28" = 7, "A33" = 262, "A28&A33" = 34))
+
+plot(myEuler, lty=1:3, quantities=TRUE)
+
+#man, euler plots don't look as cool with just two circles (unless i wanna add in d22?)
+#not sure if that would be appropriate tho b/c its comparing to a different baseline?
+
+###### make a scatterplot of responses to A28/33 when copepods develop at 18 #####
+
+# contrast D18_A28 vs D18_A33
+res_D18_BASEvsA28 <- as.data.frame(results(dds, contrast=c("group","D18BASE","D18A28"), alpha = 0.05))
+
+#contrast D22_A28 vs BASE
+res_D18_BASEvsA33 <- as.data.frame(results(dds, contrast=c("group","D18BASE","D18A33"), alpha = 0.05))
+
+#merge dfs
+res_dfD18 <- merge(res_D18_BASEvsA28, res_D18_BASEvsA33, by = "row.names", suffixes = c(".28",".33"))
+
+#putting row.names back as true rownames and then deleting
+rownames(res_dfD18) <- res_dfD18$Row.names
+res_dfD18 <- res_dfD18[,-1]
+
+#define colormapping logic with the mutate function
+
+res_dfD18 <- res_dfD18 %>%  
+  mutate(fill = case_when(
+    padj.28 < 0.05 & stat.28 < 0 ~ "turquoise2",
+    padj.28 < 0.05 & stat.28 > 0 ~ "magenta",
+    padj.33 < 0.05 & stat.33 < 0 ~ "blue2",
+    padj.33 < 0.05 & stat.33 > 0 ~ "red"
+  ))
+
+ggplot(res_dfD18, aes(x = log2FoldChange.28, y=log2FoldChange.33, color = fill)) +
+  geom_point(alpha=0.8) +
+  scale_color_identity() +
+  labs(x= "Log2FoldChange 28 vs. BASE",
+       y= "Log2FoldChange 33 vs. BASE",
+       title= "How does response vary by acute temp?") +
+  theme_minimal()
+
+#cool!
+
+color_counts <- res_dfD18 %>% 
+  group_by(fill) %>% 
+  summarise(count=n())
+
+label_positions <- data.frame(
+  fill= c("blue", "magenta", "red", "turquoise2"),
+  x_pos = c(1,5,0,-7.5),
+  y_pos = c(-5, 0, 9, 3)
+)
+
+label_data <- merge(color_counts, label_positions, by = "fill")
+
+plotD18 <- ggplot(res_dfD18, aes(x = log2FoldChange.28, y=log2FoldChange.33, color = fill)) +
+  geom_point(alpha=0.8) +
+  scale_color_identity() +
+  geom_text(data = label_data, aes(x=x_pos, y= y_pos, label = count, color = fill), size=5)+
+  geom_abline(intercept = 0, slope=1, linetype = "dashed", color ="black")+
+  geom_abline(intercept = 0, slope=-1, linetype= "dashed", color = "grey")+
+  xlim(-10,10) + ylim(-10,10)+
+  labs(x= "Log2FoldChange 28 vs. BASE",
+       y= "Log2FoldChange 33 vs. BASE",
+       title= "How does response vary by acute temp?") +
+  theme_minimal()
+
+plotD18
+
+#cool, but like before, the lower label is just not showing up. also no scripts were posted from this day :/
+#looks like this is will be a job for ppt text boxes lol
+
+#interp: upper left quadrant: upregged in 33, downregged in 28
+# upper right: upregged in both 33 and 28
+# lower right: downregged in 33, upregged in 28
+# lower left: downregged in 33, downregged in 33
+
+#be sure about the polarity of this! it might be opposite.
+#okay after a lotta digging it looks like the opposite is true. 
+#I'm hitting the hay for now, but in the morning I'll have to 
+#reverse the order on all the contrast statments.
